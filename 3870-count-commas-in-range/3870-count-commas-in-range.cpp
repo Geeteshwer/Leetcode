8aac229b139e541eb53767d0af0015a6e8650803
Geeteshwer Vashishth <geeteshwers@gmail.com>
2026-09-08 08:36:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int ans = 0;
-        for(int i =1000;i<=n;i++){
-            ans++;
+        if(n>=1000){
+        return n-1000+1;
         }
-        return ans;
+        return 0;
     }
 };
